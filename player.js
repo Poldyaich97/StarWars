2234fe37fca86c3af72acb949624e5518690c2a8
Poldyaich <daniil.poldyaev97@gmail.com>
2021-10-15 22:29:07 +0500

    Внесли логику связанную с игроком в объект игрока

--- player.js
+++ player.js
@@ -1,38 +1,3 @@
-export function checkPlayersConditions(x, y, width, height, playerW) {
-  let newX = x;
-  let newY = y;
-  if (newX > width - playerW) {
-    newX = 0;
-  }
-  if (newX < 0) {
-    newX = width - playerW;
-  }
-  if (newY > height - playerW) {
-    newY = 0;
-  }
-  if (newY < 0) {
-    newY = height - playerW;
-  }
-  let coordinates = {
-    x: newX,
-    y: newY,
-  };
-  return coordinates;
-}
-
-export function drawPlayer(ctx, x, y, playerW, playerH, image) {
-  ctx.drawImage(image, 0, 0, playerW, playerH, x, y, playerW, playerH);
-}
-export function updatePlayerCoordinates(x, y, speedX, speedY) {
-  let newX = x + speedX;
-  let newY = y + speedY;
-  let coordinates = {
-    x: newX,
-    y: newY,
-  };
-  return coordinates;
-}
-
 export function Player() {
   this.width = 32;
   this.height = 32;
@@ -41,4 +6,35 @@ export function Player() {
   this.speed = 30;
   this.x = 50;
   this.y = 0;
+  this.updateCoordinates = function () {
+    this.x = this.x + this.speedX;
+    this.y = this.y + this.speedY;
+  };
+  this.draw = function (ctx, image) {
+    ctx.drawImage(
+      image,
+      0,
+      0,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  };
+  this.checkConditions = function (width, height) {
+    if (this.x > width - this.width) {
+      this.x = 0;
+    }
+    if (this.x < 0) {
+      this.x = width - this.width;
+    }
+    if (this.y > height - this.width) {
+      this.y = 0;
+    }
+    if (this.y < 0) {
+      this.y = height - this.width;
+    }
+  };
 }
